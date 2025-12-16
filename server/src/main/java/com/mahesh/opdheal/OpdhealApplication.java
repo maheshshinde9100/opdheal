099acdadd9b21d809fe3992861987d9bb9b2265a
Mahesh Shinde <maheshshinde9100@gmail.com>
2025-12-16 12:56:00 +0530

    initial project structure with Maven configuration and Spring Boot application commit for the OPDHeal project

---
+++ server/src/main/java/com/mahesh/opdheal/OpdhealApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.opdheal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpdhealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpdhealApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/mahesh/opdheal/OpdhealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.opdheal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpdhealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
